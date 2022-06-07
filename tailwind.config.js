cf911995e94fd0a924836ce8dfa8b273c8806f51
Preetam04 <preetampatil2324@gmail.com>
2022-06-07 15:38:25 +0530

    New github Acc setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'NetflixSansRg': ['NetflixSansRg', 'sans-serif'],
+        'NetflixSansMd': ['NetflixSansMd', 'sans-serif']
+      },
+      backgroundImage: {
+        'image': "url('/assets/images/banner2.jpg')",
+      },
+      colors: {
+        'customRed': '#e50914',
+      }
+    },
+  },
+  plugins: [],
+}
